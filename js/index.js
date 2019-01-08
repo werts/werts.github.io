2a1e7bb60af4cbb367e82d4c1ec3a06ade06e8db
weiyouqiang <3224330600@qq.com>
2019-01-08 13:26:54 +0800

    init

--- js/index.js
+++ js/index.js
@@ -1,54 +1,54 @@
 (function(win, doc) {
-	var indexTypeWriter = new Typewriter('#content', {
-		loop: false,
-		delay:2
-	});
-	var data = doc.getElementById('posts').innerHTML;
-	
-	var fix = function(num){
-		return num<10?`0${num}`:`${num}`;
-	};
-	
-	var formatTime = function(time){
-		var date = new Date(time);
-		
-		return `${date.getFullYear()}-${fix(date.getMonth()+1)}-${fix(date.getDate())}`;
-	};
+    var indexTypeWriter = new Typewriter('#content', {
+        loop: false,
+        delay: 20
+    });
+    var data = doc.getElementById('posts').innerHTML;
 
-	try {
-		data = data.split('n+br');
+    var fix = function(num) {
+        return num < 10 ? `0${num}` : `${num}`;
+    };
 
-		if (!data.length || data.length === 1) return;
-		
-		if (data.length>20) data.length = 20; //最多20条
+    var formatTime = function(time) {
+        var date = new Date(time);
 
-		for (var i = 0; i < data.length - 1; i++) {
-			var current = data[i];
-			
-			current = JSON.parse(current);
-			
-			indexTypeWriter.typeString('>  ')
-			.typeString(`<span class="fold">${formatTime(current.time)}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`)
-			.typeString(`<a href="${current.url}" class="command">${current.title}</a>`);
-			
-			if (current.tags){
-				var tags = current.tags.split(',');
-				
-				for (var j = 0;j<tags.length;j++){
-					indexTypeWriter.typeString(`&nbsp;&nbsp;<span class="tag">${tags[j]}</span>`);
-				}
-			}
-			
-			if (i !== data.length-2){
-				indexTypeWriter.pauseFor(3);
-				indexTypeWriter.typeString('<br/>');
-			}else{
-				indexTypeWriter.stop();
-			}
-		}
-		
-		indexTypeWriter.start();
-	} catch (e) {
-		//TODO handle the exception
-	}
+        return `${date.getFullYear()}-${fix(date.getMonth()+1)}-${fix(date.getDate())}`;
+    };
+
+    try {
+        data = data.split('n+br');
+
+        if (!data.length || data.length === 1) return;
+
+        if (data.length > 20) data.length = 20; //最多20条
+
+        for (var i = 0; i < data.length - 1; i++) {
+            var current = data[i];
+
+            current = JSON.parse(current);
+
+            indexTypeWriter.typeString('>  ')
+                .typeString(`<span class="fold">${formatTime(current.time)}</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`)
+                .typeString(`<a href="${current.url}" class="command">${current.title}</a>`);
+
+            if (current.tags) {
+                var tags = current.tags.split(',');
+
+                for (var j = 0; j < tags.length; j++) {
+                    indexTypeWriter.typeString(`&nbsp;&nbsp;<span class="tag">${tags[j]}</span>`);
+                }
+            }
+
+            if (i !== data.length - 2) {
+                indexTypeWriter.pauseFor(3);
+                indexTypeWriter.typeString('<br/>');
+            } else {
+                indexTypeWriter.stop();
+            }
+        }
+
+        indexTypeWriter.start();
+    } catch (e) {
+        //TODO handle the exception
+    }
 }(window, document));
